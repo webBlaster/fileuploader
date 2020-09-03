32a0a2a3f28fe9705afba114b54e93ed6259fc1f
react native doctor <yekinnijibola@gmail.com>
2020-09-04 00:34:32 +0100

    imports for crud

--- model/classes.py
+++ model/classes.py
@@ -1,3 +1,10 @@
+  
+#security imports
+from werkzeug.security import generate_password_hash, check_password_hash
+#imports for json
+from flask import jsonify
+#database imports
+from database_setup import db, Admin as admin, Picture as picture
 class Admin():
     def signup(self):
         pass
