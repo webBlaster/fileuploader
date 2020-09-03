e7a2390abd0d73d1a4e4e6808fd2feb55edeab8e
react native doctor <yekinnijibola@gmail.com>
2020-09-03 21:09:45 +0100

    added more dependencies

---
+++ model/classes.py
@@ -0,0 +1,16 @@
+class Admin():
+    def signup(self):
+        pass
+    def login(self):
+        pass
+    def logout(self):
+        pass
+
+
+class Picture():
+    def upload_image(self):
+        pass
+    def delete_image(self):
+        pass
+    def get_all_images(self):
+        pass
