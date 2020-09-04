a4f66c701c7f57db31f8409b690d8a53ac198866
react native doctor <yekinnijibola@gmail.com>
2020-09-04 20:05:48 +0100

    refactored controller

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ from flask import Flask
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'JFDDJjddjffjfffj88-999*h:dkdjffff'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
