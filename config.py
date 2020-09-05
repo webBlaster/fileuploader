05d9f1af3c1471703b4a41d2642818eae5b7ef1c
react native doctor <yekinnijibola@gmail.com>
2020-09-05 21:18:13 +0100

    added cors to endpoint

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 from flask import Flask
 import cloudinary
 import os
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 app.config['SECRET_KEY'] = 'JFDDJjddjffjfffj88-999*h:dkdjffff'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
