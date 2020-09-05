ae2df4541d59298d19e93c96e1ee0b5aebbbf549
react native doctor <yekinnijibola@gmail.com>
2020-09-05 11:12:44 +0100

    added more environmental viarables

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
 from flask import Flask
+import cloudinary
+import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'JFDDJjddjffjfffj88-999*h:dkdjffff'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+cloudinary.config( 
+  cloud_name = os.environ.get('CLOUD_NAME'), 
+  api_key = os.environ.get('CLOUD_API_KEY'), 
+  api_secret = os.environ.get('CLOUD_API_SECRET') 
+)
