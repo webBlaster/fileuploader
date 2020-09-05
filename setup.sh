e83f0ed3de10b0f4288f756315bbaa4012e8b28e
react native doctor <yekinnijibola@gmail.com>
2020-09-05 20:45:12 +0100

    added postgres

--- setup.sh
+++ setup.sh
@@ -6,4 +6,7 @@ export CLOUD_NAME=webblaster
 export CLOUD_API_KEY=435854196983661
 export CLOUD_API_SECRET=J_82bJf3irUzd9aYaQMjMD98QtA
 
+#database
+export DATABASE_URL=postgres://uloptrxlevybej:4e9942987b58006a0ae41f2702778c034c6fe35896037d77c503d355a24f71f0@ec2-18-209-187-54.compute-1.amazonaws.com:5432/da8vm3pfoloosi
+
 echo "done setting up environ vars"
